aaafe2eaf5f676ae858771167e1b0655865af6b2
nqtinh <37231358+nqtinh@users.noreply.github.com>
2018-06-04 10:08:08 +0700

    add

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,3 +4,6 @@ var express_handlebars_sections = require('express-handlebars-sections');
 var bodyParser = require('body-parser');
 var path = require('path');
 var wnumb = require('wnumb');
+
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);

d6fdf97cb12a5a9e005f45065cd822905e6ae453
nqtinh <37231358+nqtinh@users.noreply.github.com>
2018-06-06 22:26:45 +0700

    full hdb

--- Back-end/app.js
+++ Back-end/app.js
@@ -7,3 +7,36 @@ var wnumb = require('wnumb');
 
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
+var homeController = require('./controllers/homeController');
+
+var app = express();
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: 'views/_layouts/',
+    helpers: {
+        section: express_handlebars_sections(),
+        number_format: n => {
+            var nf = wnumb({
+                thousand: ','
+            });
+            return nf.to(n);
+        }
+    }
+}));
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+app.use('/home', homeController);
+
+app.listen(3000, () => {
+    console.log('Site running on port 3000');
+});

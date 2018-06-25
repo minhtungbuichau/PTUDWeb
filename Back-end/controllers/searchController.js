2fb0256032990309f284c695f415ec0c72cadcd5
Ken Nguyen <30380103+thelannisters@users.noreply.github.com>
2018-06-25 13:42:22 +0700

    search-version-0.0.4
    thêm tính năng tìm kiếm bằng tên loại và tên nhà sản xuất

--- Back-end/controllers/searchController.js
+++ Back-end/controllers/searchController.js
@@ -4,6 +4,43 @@ var searchRepo = require('../repos/searchRepo');
 var config = require('../config/config');
 var router = express.Router();
 var urlencoded = bodyParser.urlencoded({extended:false});
+router.get('/resultAll',function(req,res){
+    var page = req.query.page;
+    if (!page) {
+        page = 1;
+    }
+
+    var offset = (page - 1) * config.PRODUCTS_PER_PAGE;
+    var p1 = searchRepo.loadAll(offset);
+    var p2 = searchRepo.countAll();
+    
+    Promise.all([p1, p2]).then(([pRows, countRows]) => {
+        var total = countRows[0].total;
+        var nPages = total / config.PRODUCTS_PER_PAGE;
+        if (total % config.PRODUCTS_PER_PAGE > 0) {
+            nPages++;
+        }
+
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurPage: i === +page
+            });
+        }
+
+        var vm = {
+            
+            searchItem: pRows,
+            noProducts: pRows.length === 0,
+            page_numbers: numbers
+        };
+        
+            res.render('search/resultAll', vm);
+  
+        
+    });
+})
 router.get('/resultProName/:proName',function(req,res){
     var proName = req.params.proName;
     var page = req.query.page;
@@ -47,9 +84,130 @@ router.get('/resultProName/:proName',function(req,res){
         
     });
 })
+//router search Catname
+router.get('/resultCatName/:catName',function(req,res){
+    var catName = req.params.catName;
+    var page = req.query.page;
+    if (!page) {
+        page = 1;
+    }
+
+    var offset = (page - 1) * config.PRODUCTS_PER_PAGE;
+    var p1 = searchRepo.loadbyCatName(catName,offset);
+    var p2 = searchRepo.countByCatName(catName);
+    
+    Promise.all([p1, p2]).then(([pRows, countRows]) => {
+        var total = countRows[0].total;
+        var nPages = total / config.PRODUCTS_PER_PAGE;
+        if (total % config.PRODUCTS_PER_PAGE > 0) {
+            nPages++;
+        }
+
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurPage: i === +page
+            });
+        }
+
+        var vm = {
+            
+            searchItem: pRows,
+            noProducts: pRows.length === 0,
+            page_numbers: numbers
+        };
+        console.log(pRows);
+        if(pRows[0] != null)
+        {
+            res.render('search/resultCatName', vm);
+            
+        }
+        
+        else{
+            res.render('search/resultNothing');
+        }
+        
+    });
+})
+
+//router search ProduName
+router.get('/resultProduName/:ProduName',function(req,res){
+    var ProduName = req.params.ProduName;
+    var page = req.query.page;
+    if (!page) {
+        page = 1;
+    }
+
+    var offset = (page - 1) * config.PRODUCTS_PER_PAGE;
+    var p1 = searchRepo.loadbyProduName(ProduName,offset);
+    var p2 = searchRepo.countByProduName(ProduName);
+    
+    Promise.all([p1, p2]).then(([pRows, countRows]) => {
+        var total = countRows[0].total;
+        var nPages = total / config.PRODUCTS_PER_PAGE;
+        if (total % config.PRODUCTS_PER_PAGE > 0) {
+            nPages++;
+        }
+
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurPage: i === +page
+            });
+        }
+
+        var vm = {
+            
+            searchItem: pRows,
+            noProducts: pRows.length === 0,
+            page_numbers: numbers
+        };
+        console.log(pRows);
+        if(pRows[0] != null)
+        {
+            res.render('search/resultProduName', vm);
+            
+        }   
+        else{
+            res.render('search/resultNothing');
+        }
+        
+    });
+})
+
+
 var urlencoded = bodyParser.urlencoded({extended:false});
 router.post('/action',urlencoded,function(req,res){
-    var proName = req.body.searchEle;
-    res.redirect('/search/resultProName/'+proName);
+    var keywork = req.body.searchEle;
+    var valueSearch = req.body.option;
+    if(valueSearch == 0)
+    {
+        if(keywork ===''){
+            res.redirect('/search/resultAll');
+        }
+        else{
+            res.redirect('/search/resultProName/'+keywork);
+        }
+    }
+    else if(valueSearch == 1)
+    {
+        if(keywork ===''){
+            res.redirect('/search/resultNothing');
+        }
+        else{
+            res.redirect('/search/resultCatName/'+keywork);
+        }
+    }
+    else {
+        if(keywork ===''){
+            res.redirect('/search/resultNothing');
+        }
+        else{
+            res.redirect('/search/resultProduName/'+keywork);
+        }
+    }
+    
 });
 module.exports = router;
--- Back-end/repos/searchRepo.js
+++ Back-end/repos/searchRepo.js
@@ -1,10 +1,13 @@
 var db = require('../fn/db');
 var config = require('../config/config');
-exports.loadAll = () => {
-    var sql = 'select * from products';
+exports.loadAll = (offset) => {
+    var sql =`select * from products limit ${config.PRODUCTS_PER_PAGE} offset ${offset}`;
     return db.load(sql);
 }
-
+exports.countAll = ()=>{
+    var sql = `select count(*) as total from products `;
+    return db.load(sql);
+}
 exports.loadbyName = (proName,offset)=>{
     var sql = `select * from products where ProName like "%${proName}%" limit ${config.PRODUCTS_PER_PAGE} offset ${offset} `;
     return db.load(sql);
@@ -15,3 +18,23 @@ exports.countByPro = proName => {
     return db.load(sql);
 }
 
+//load by catname
+exports.loadbyCatName = (catName,offset)=>{
+    var sql = `select products.*, categories.CatName from products , categories where products.CatID = categories.CatID AND categories.CatName like "%${catName}%" limit ${config.PRODUCTS_PER_PAGE} offset ${offset} `;
+    return db.load(sql);
+}
+
+exports.countByCatName = catName => {
+	var sql = `select count(products.ProID) as total from products, categories where products.CatID = categories.CatID AND CatName like "%${catName}%"`;
+    return db.load(sql);
+}
+//Produname
+exports.loadbyProduName = (ProduName,offset)=>{
+    var sql = `select products.*, producers.ProduName from products , producers where products.ProduID = producers.ProduID AND producers.ProduName like "%${ProduName}%"  limit ${config.PRODUCTS_PER_PAGE} offset ${offset} `;
+    return db.load(sql);
+}
+
+exports.countByProduName= ProduName => {
+	var sql = `select count(products.ProID) as total from products, producers where products.ProduID = producers.ProduID AND ProduName like "%${ProduName}%"`;
+    return db.load(sql);
+}

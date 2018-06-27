4838d11b6a8a6201b5ef4f421155ae73ea20b0dd
hqtrungg <32814200+hqtrungg@users.noreply.github.com>
2018-06-27 14:45:55 +0700

    aa

--- Back-end/controllers/adminController.js
+++ Back-end/controllers/adminController.js
@@ -4,16 +4,8 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/admin/dashbord', (req, res) => {
+router.get('/', (req, res) => {
     res.render('admin/admin');
-});
-
-router.get('/admin/show-by-category', (req, res) => {
-    res.render('admin/byCatAdmin');
-});
-
-router.get('/admin/show-by-producer', (req, res) => {
-    res.render('admin/byProduAdmin');
 });
 
 module.exports = router;
